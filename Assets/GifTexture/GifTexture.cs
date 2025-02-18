566d9cf38fdb9c832a458423b4a22e8c704c54a4
Blazing <161096238+Blazing-Cox@users.noreply.github.com>
2025-02-18 08:24:11 -0600

    Scene Added

---
+++ Assets/GifTexture/GifTexture.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Jillzhang.GifUtility;
+using System.IO;
+
+public class GifTexture
+{
+    private static DummyBehaviour host;
+    private static DummyBehaviour Host
+    {
+        get
+        {
+            if (host == null)
+            {
+                GameObject go = new GameObject("{Gif Host}");
+                host = go.AddComponent<DummyBehaviour>();
+                go.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return host;
+        }
+    }
+
+
+    private GifImage gif;
+    private int index;
+
+    public GifTexture(string filename)
+    {
+        gif = GifDecoder.Decode(filename);
+        if (gif == null)
+        {
+            throw new System.Exception("Could not decode gif");
+        }
+        SetFirstFrame();
+    }
+
+    public GifTexture(byte[] buffer)
+    {
+        gif = GifDecoder.Decode(buffer);
+        if (gif == null)
+        {
+            throw new System.Exception("Could not decode gif");
+        }
+        SetFirstFrame();
+    }
+
+    public bool UseRealtime { get; set; }
+
+    private Coroutine coroutine;
+    private bool isPlaying;
+    public bool IsPlaying
+    {
+        get
+        {
+            return isPlaying;
+        }
+    }
+
+    //TODO:  Get App Extension for looping, read the value and play exact number of times
+    private bool loop;
+    public bool Loop
+    {
+        get
+        {
+            return loop;
+        }
+        set
+        {
+            loop = value;
+        }
+    }
+
+    public float Duration {
+        get
+        {
+            float dur = 0f;
+            for (int i = 0; i < gif.Frames.Count; i++)
+            {
+                dur += gif.Frames[i].Delay / 100f;
+            }
+            return dur;
+        }
+    }
+
+    public void Pause()
+    {
+        if (isPlaying)
+        {
+            Host.StopCoroutine(coroutine);
+            isPlaying = false;
+        }
+    }
+
+    public void Play()
+    {
+        if (!isPlaying)
+        {
+            if (index == gif.Frames.Count -1)
+            {
+                index = 0;
+            }
+            coroutine = Host.StartCoroutine(CycleGif());
+            isPlaying = true;
+        }
+    }
+
+    public void Stop()
+    {
+        Pause();
+        SetFirstFrame();
+    }
+
+    IEnumerator CycleGif()
+    {
+        while (true)
+        {
+            gif.UpdateTexture(index);
+            if (UseRealtime)
+            {
+                yield return new WaitForSecondsRealtime(gif.Frames[index].Delay / 100f);
+            }
+            else
+            {
+                yield return new WaitForSeconds(gif.Frames[index].Delay / 100f);
+            }
+
+            if (loop || index < gif.Frames.Count - 1)
+            {
+                index = (index + 1) % gif.Frames.Count;
+            }
+            else
+            {
+                break;
+            }
+        }
+        isPlaying = false;
+    }
+
+
+    void SetFirstFrame()
+    {
+        if(gif.Frames.Count > 0)
+        {
+            index = 0;
+            gif.UpdateTexture(index);
+        }
+    }
+    static public implicit operator Texture(GifTexture gifTexture)
+    {
+        if(gifTexture == null || gifTexture.gif == null)
+        {
+            return null;
+        }
+        return gifTexture.gif.Texture;
+    }
+
+    private class DummyBehaviour : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/Scripts/Gif_Textures/GifTextureTest.cs
@@ -0,0 +1,34 @@
+﻿using Jillzhang.GifUtility;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class GifTextureTest : MonoBehaviour {
+
+    public Renderer Renderer;
+
+    GifTexture gifTexture;
+    // Use this for initialization
+    void Start()
+    {
+        byte[] buffer = System.IO.File.ReadAllBytes("Assets/Materials/Brain_Dome/demo.gif");
+        gifTexture = new GifTexture(buffer);
+        gifTexture.Loop = true;
+        gifTexture.Play();
+        Renderer.material.mainTexture = gifTexture;
+    }
+
+    public void Pause()
+    {
+        gifTexture.Pause();
+    }
+    public void Play()
+    {
+        gifTexture.Play();
+    }
+    public void Stop()
+    {
+        gifTexture.Stop();
+    }
+}
